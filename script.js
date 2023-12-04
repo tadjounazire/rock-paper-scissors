48e6e8f4bcd94a40ca1033bfdc30f0fbf8d53e18
tadjounazire <abdoulakram6@gmail.com>
2023-12-05 06:04:37 +1400

    committed rps-ui branch

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3) + 1;
-    if (computerChoice === 1){
+    if (computerChoice == 1){
         return "rock";
-    }else if (computerChoice === 2){
+    }else if (computerChoice == 2){
         return "paper";
     }else{
         return "scissors";
@@ -10,12 +10,12 @@ function getComputerChoice(){
 }
 
 function checkWinner(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
+    if (playerSelection == computerSelection){
         return "tie"
     }else if(
-      (playerSelection === "paper" && computerSelection === "rock") ||
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "scissors" && computerSelection === "paper")
+      (playerSelection == "paper" && computerSelection == "rock") ||
+      (playerSelection == "rock" && computerSelection == "scissors") ||
+      (playerSelection == "scissors" && computerSelection == "paper")
     ){
             return "Player";
     }else{
@@ -34,44 +34,36 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function getPlayerChoice(){
-    let validated = false;
-    while(validated == false){
-        const choice = prompt("Enter rock or paper or scissors");
-        if (choice == null){
-            continue;
-        }
-        const choiceInLowerCase = choice.toLowerCase();
-        if (choiceInLowerCase == "rock" || choiceInLowerCase == "paper" || choiceInLowerCase == "scissors"){
-            validated = true;
-            return choiceInLowerCase;
-        }
-    }
-}
-
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    console.log("Welcome!")
-    for (let i = 0;i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("-------------------")
+const buttons = document.querySelectorAll("button");
+let playerScore = 0;
+let computerScore = 0;
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.className;
+        let computerSelection = getComputerChoice();
+        const resultDisplay = document.querySelector(".results");
+        const player = document.createElement("p");
+        const computer = document.createElement("p");
         if (checkWinner(playerSelection, computerSelection) == "Player"){
-            playerScore++;
+            playerScore += 1; 
+            player.textContent = `Your score is ${playerScore}`
         }else if(checkWinner(playerSelection, computerSelection) == "Computer"){
-            computerScore++;
+            computerScore += 1;
+            computer.textContent = `The computer's score is ${computerScore}`
         }
-    }
-    console.log("Game Over");
-    if (playerScore > computerScore){
-        console.log("You won the game!!!");
-    }else if(playerScore < computerScore){
-        console.log("The computer won the game!!!");
-    }else {
-        console.log("The game was a tie");
+        const result = document.createElement("p");
+        result.textContent = `${playRound(playerSelection, computerSelection)}`
+        resultDisplay.appendChild(player);
+        resultDisplay.appendChild(computer);
+        resultDisplay.appendChild(result); 
+        endGame();
+    })
+})
+
+function endGame(){
+    if (playerScore == 5){
+        document.write(`Your score is ${playerScore}, The computer's score is ${computerScore}, You won`)
+    }else if(computerScore == 5){
+        document.write(`The computer scored ${computerScore},  You scored ${playerScore}, The computer won`)
     }
 }
-
-game();
